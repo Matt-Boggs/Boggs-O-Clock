bdc410fd38f3685b45202f2fc6444a1f5004ed50
Matt-Boggs <matt.david.boggs@gmail.com>
2021-07-08 13:36:55 -0600

    Add files via upload

---
+++ BoggsOhClock/BoggsOhClock/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace BoggsOhClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TimeDisplayBox = new System.Windows.Forms.TextBox();
+            this.ClockPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ClockPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TimeDisplayBox
+            // 
+            this.TimeDisplayBox.Location = new System.Drawing.Point(675, 12);
+            this.TimeDisplayBox.Name = "TimeDisplayBox";
+            this.TimeDisplayBox.Size = new System.Drawing.Size(120, 20);
+            this.TimeDisplayBox.TabIndex = 0;
+            // 
+            // ClockPanel
+            // 
+            this.ClockPanel.Controls.Add(this.label2);
+            this.ClockPanel.Controls.Add(this.label1);
+            this.ClockPanel.Location = new System.Drawing.Point(217, 51);
+            this.ClockPanel.Name = "ClockPanel";
+            this.ClockPanel.Size = new System.Drawing.Size(300, 300);
+            this.ClockPanel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "12";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ClockPanel);
+            this.Controls.Add(this.TimeDisplayBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ClockPanel.ResumeLayout(false);
+            this.ClockPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TimeDisplayBox;
+        private System.Windows.Forms.Panel ClockPanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BoggsOhClock/BoggsOhClock/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoggsOhClock
+{
+    public partial class Form1 : Form
+    {
+        public int MidX { get; set; }
+        public int MidY { get; set; }
+        public Point MidPt { get; set; }
+        public Point DrawPt { get; set; }
+        public Double Angle { get; set; }
+        public int Length { get; set; }
+        public Graphics ClockGraphics { get; set; }
+        public Pen PenW { get; set; }
+        public Pen PenB { get; set; }
+        public Pen PenR { get; set; }
+        public Pen PenHr { get; set; }
+        public Brush Brush { get; set; }
+        public Rectangle Rectangle { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Set up and start the timer
+            Timer ticktock = new Timer
+            {
+                Interval = 1000
+            };
+            ticktock.Tick += Ticktock_Tick;
+            ticktock.Start();
+            //ClockPanel.BackColor = Color.White;
+
+            //Set up DrawLine parameters, Angle will be passed around for the three clock hands
+            Length = ClockPanel.Height / 2;
+            //Angle will be in radians (2 Radians is one circle)
+            Angle = 0;
+            MidX = ClockPanel.Width / 2;
+            MidY = ClockPanel.Height / 2;
+            MidPt = new Point(MidX, MidY);
+            DrawPt = new Point(Convert.ToInt32(MidX + Math.Cos(Angle) * Length), Convert.ToInt32(MidY + Math.Sin(Angle) * Length));
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //Set up graphics area
+            ClockGraphics = ClockPanel.CreateGraphics();
+            ClockGraphics.SmoothingMode = SmoothingMode.HighQuality;
+            Rectangle = ClockPanel.ClientRectangle;
+
+            //Set up Pens for hands, brush for bg
+            PenB = new Pen(Color.Black,2);
+            PenHr = new Pen(Color.Black, 4);
+            PenW = new Pen(Color.White, 4);
+            PenR = new Pen(Color.Red, 2);
+            Brush = Brushes.White;
+
+            //Fill in bg
+            ClockGraphics.FillEllipse(Brush, Rectangle);
+            ClockGraphics.DrawEllipse(PenB, Rectangle);
+            //ClockGraphics.DrawLine(Pen,MidPt,DrawPt);
+
+        }
+
+        private void Ticktock_Tick(object sender, EventArgs e)
+        {
+            //Start with a whitefilled circle, then draw the hands on top of that
+            ClockGraphics.FillEllipse(Brush, Rectangle);
+            ClockGraphics.DrawEllipse(PenB, Rectangle);
+
+            //TextBox Display
+            var time = DateTime.Now;
+            TimeDisplayBox.Text = time.ToString();
+
+            //Draw hour hand
+            //ClockGraphics.DrawLine(PenW, MidPt, DrawPt); //This method originally drew a white line over the previously made line
+                    //30 : 360dg / 12hr = 30 dg
+            Angle = ((30 * time.Hour - 90) * Math.PI / 180);
+            DrawPt = new Point(Convert.ToInt32(MidX + Math.Cos(Angle) * Length), Convert.ToInt32(MidY + Math.Sin(Angle) * Length));
+            ClockGraphics.DrawLine(PenHr, MidPt, DrawPt);
+
+            //Draw minute hand
+            //ClockGraphics.DrawLine(PenW, MidPt, DrawPt);
+                    //6 : 360dg / 60Min = 6dg
+            Angle = ((6 * time.Minute - 90) * Math.PI / 180);
+            DrawPt = new Point(Convert.ToInt32(MidX + Math.Cos(Angle) * Length), Convert.ToInt32(MidY + Math.Sin(Angle) * Length));
+            ClockGraphics.DrawLine(PenB, MidPt, DrawPt);
+
+            //Draw Second Hand
+            //ClockGraphics.DrawLine(PenW, MidPt, DrawPt);
+                    //6 : 360dg / 60Sec = 6dg   
+            Angle = ((6 * time.Second - 90) * Math.PI / 180);
+            DrawPt = new Point(Convert.ToInt32(MidX + Math.Cos(Angle) * Length), Convert.ToInt32(MidY + Math.Sin(Angle) * Length));
+            ClockGraphics.DrawLine(PenR, MidPt, DrawPt);
+
+        }
+
+    }
+}
